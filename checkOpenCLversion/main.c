047a43aaa9e35e7dd19adc81a740ea5d2c26be15
sergeyMelentyev <sergey.melentyev@gmail.com>
2016-02-05 21:15:15 +0300

    Initial Commit

---
+++ checkOpenCLversion/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  checkOpenCLversion
+//
+//  Created by Olga on 05/02/16.
+//  Copyright © 2016 Sergey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
